2f3eb1a4ec6410b7d3b0e1199e032a41b5e342b6
jarrodolson <jarrod.olson@outlook.com>
2014-02-17 21:51:40 -0800

    Added some date functions to data ingest and madnessStart variable to season.csv

--- _script-dataIngest.R
+++ _script-dataIngest.R
@@ -3,24 +3,6 @@ library(reshape)
 library(dplyr)
 options(stringsAsFactors=FALSE)
 
-regSeason <- read.csv("Data\\regular_season_results.csv",header=TRUE)
-seasons <- read.csv("Data\\seasons.csv",header=TRUE)
-tourneyResults <- read.csv("Data\\tourney_results.csv",header=TRUE)
-tourneySeeds <- read.csv("Data\\tourney_seeds.csv",header=TRUE)
-tourneySlots <- read.csv("Data\\tourney_slots.csv",header=TRUE)
-
-seasons$dayzero <- as.POSIXct(strptime(seasons$dayzero,format="%m/%d/%Y"))
-
-history <- read.delim("Data\\HistoricTourneyResults.txt",sep="\t",header=TRUE)
-history$game <- paste(history$Year,history$Team1,history$Team2,sep="_")
-history$RankDiff <- history$Team1Rank-history$Team2Rank
-history$ScoreDiff <- history$Team1Score-history$Team2Score
-##Need to build out model showing probability of a win given ranking, then given ranking
-##  considering opponent rank
-
-teamID <- read.csv("Data\\TeamID.csv",header=TRUE)
-teamID$name <- NULL
-teamID <- filter(teamID,Team!="")
 
 bringInTeams <- function(){
   teams_other <- read.delim("Data\\teamStats_2014Feb16.txt",sep="\t",header=TRUE,stringsAsFactors=FALSE)
@@ -32,10 +14,10 @@ getTeamDyads <- function(){
   tempTeamID <- teamID
   tempTeamID$name <- NULL
   tempTeamID1 <- rename(tempTeamID,
-                       c(Team="team_play",
-                         Conference="conf_play",
-                         IDNumber="IDNum_play",
-                         id="id_play"))  
+                        c(Team="team_play",
+                          Conference="conf_play",
+                          IDNumber="IDNum_play",
+                          id="id_play"))  
   teamDyads <- merge(teamDyads,tempTeamID1,by.x="Team",by.y="team_play",all.x=TRUE)
   tempTeamID <- rename(tempTeamID,
                        c(Team="team_oppo",
@@ -48,7 +30,41 @@ getTeamDyads <- function(){
   return(teamDyads)
 }
 
+
+regSeason <- read.csv("Data\\regular_season_results.csv",header=TRUE)
+seasons <- read.csv("Data\\seasons.csv",header=TRUE)
+tourneyResults <- read.csv("Data\\tourney_results.csv",header=TRUE)
+tourneySeeds <- read.csv("Data\\tourney_seeds.csv",header=TRUE)
+tourneySlots <- read.csv("Data\\tourney_slots.csv",header=TRUE)
+
+seasons$dayzero <- as.POSIXct(strptime(seasons$dayzero,format="%m/%d/%Y"))
+##Note: Seasons$madnessStart manually coded from Wikipedia based on Kaggle "seasons.csv"
+seasons$madnessStart <- as.POSIXct(strptime(seasons$madnessStart,format="%m/%d/%Y"))
+
+history <- read.delim("Data\\HistoricTourneyResults.txt",sep="\t",header=TRUE)
+history$game <- paste(history$Year,history$Team1,history$Team2,sep="_")
+history$RankDiff <- history$Team1Rank-history$Team2Rank
+history$ScoreDiff <- history$Team1Score-history$Team2Score
+##Need to build out model showing probability of a win given ranking, then given ranking
+##  considering opponent rank
+
+teamID <- read.csv("Data\\TeamID.csv",header=TRUE)
+teamID$name <- NULL
+teamID <- filter(teamID,Team!="")
+
 teams <- bringInTeams()
+
+##Need to get game dates
+teams$year <- sapply(strsplit(teams$GameName,"/"),"[",3)
+teams$month <- sapply(strsplit(teams$GameName,"/"),"[",4)
+teams$day <- sapply(strsplit(teams$GameName,"/"),"[",5)
+teams$gameDate <- as.POSIXct(paste(teams$year,teams$month,teams$day,sep="-"))
+
+##Code march madness games
+teams <- merge(teams,seasons,by.x="Season",by.y="years",all.x=TRUE)
+teams$madness <- 0
+teams$madness[teams$gameDate>=teams$madnessStart] <- 1
+
 teamDyads <- getTeamDyads()
 teamDyads <- select(teamDyads,GameName,conf_play,IDNum_play,id_play,conf_oppo,IDNum_oppo,id_oppo)
 
@@ -75,17 +91,17 @@ opponentD <- ddply(teams_def,.(IDNumber),summarize,
                    d_avgOppThreePtPer = mean(Three_Per,na.rm=TRUE))
 opponentD <- rename(opponentD, c(IDNumber="opponent_IDNumber"))
 
-##Gets us teams data merged with opponent data
-##HOWEVER: It needs to be synchronized by time to use for modeling
-teams <- merge(teams,opponentD,by.x=c("IDNum_oppo"),by.y=c("opponent_IDNumber"),all.x=TRUE)
-
-teams_off_agg <- aggregate(teams_off,by=list(teams_off$Team),FUN=mean,na.rm=TRUE)
-teams_off_agg <- teams_off_agg[c("team_IDNumber","T")]
-names(teams_off_agg)<-c("team_IDNumber","avgOffTotalPts")
-teams <- merge(teams,teams_off_agg,by=c("team_IDNumber"),all.x=TRUE)
-teams$ptsDiff <- teams$T-teams$avgOffTotalPts
-teams_off_2 <- teams[which(teams$StatType=="Offensive"),]
-library(plyr)
-defRating <- ddply(teams_off_2,.(opponent_IDNumber),summarize,meanPtsDiffAllowed=mean(ptsDiff))
-names(defRating) <- c("team_IDNumber","defRating")
-teams <- merge(teams,defRating,by=c("team_IDNumber"),all.x=TRUE)
+# ##Gets us teams data merged with opponent data
+# ##HOWEVER: It needs to be synchronized by time to use for modeling
+# teams <- merge(teams,opponentD,by.x=c("IDNum_oppo"),by.y=c("opponent_IDNumber"),all.x=TRUE)
+# 
+# teams_off_agg <- aggregate(teams_off,by=list(teams_off$Team),FUN=mean,na.rm=TRUE)
+# teams_off_agg <- teams_off_agg[c("team_IDNumber","T")]
+# names(teams_off_agg)<-c("team_IDNumber","avgOffTotalPts")
+# teams <- merge(teams,teams_off_agg,by=c("team_IDNumber"),all.x=TRUE)
+# teams$ptsDiff <- teams$T-teams$avgOffTotalPts
+# teams_off_2 <- teams[which(teams$StatType=="Offensive"),]
+# library(plyr)
+# defRating <- ddply(teams_off_2,.(opponent_IDNumber),summarize,meanPtsDiffAllowed=mean(ptsDiff))
+# names(defRating) <- c("team_IDNumber","defRating")
+# teams <- merge(teams,defRating,by=c("team_IDNumber"),all.x=TRUE)
